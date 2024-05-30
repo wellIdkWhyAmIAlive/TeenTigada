27e575e3d68a7c13d2c167e2499e56303415bf6d
wellIdkWhyAmIAlive <156097384+wellIdkWhyAmIAlive@users.noreply.github.com>
2024-05-30 19:39:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -13,16 +13,45 @@ navLinks.addEventListener("click", (e) => {
     menuBtnIcon.setAttribute("class", "ri-menu-line");
 });
 
-var swiper = new Swiper('.blog-slider', {
-    direction: "vertical",
-    spaceBetween: 30,
-    effect: 'fade',
-    loop: true,
-    mousewheel: {
-        invert: false,
-    },
-    pagination: {
-        el: '.blog-slider__pagination',
-        clickable: true,
-    },
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".header_img img", {
+    ...scrollRevealOption,
+    origin: "right",
+    delay: 200,
+});
+ScrollReveal().reveal(".header_content h1", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+ScrollReveal().reveal(".header_content .section_description", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+
+
+
+ScrollReveal().reveal(".card_container h1", {
+    ...scrollRevealOption,
+    origin: "right",
+    delay: 500,
+});
+ScrollReveal().reveal(".card_container .section_description", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+
+ScrollReveal().reveal(".memory_image", {
+    ...scrollRevealOption,
+    origin: "left",
+    delay: 200,
+});
+
+ScrollReveal().reveal(".memory_image p", {
+    ...scrollRevealOption,
+    delay: 1000,
 });
